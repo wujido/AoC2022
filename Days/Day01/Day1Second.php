52f1683e3261a5f7fbdb98692b34990d1ed90157
Vaclav Hrouda <hrouda@wpj.cz>
2022-12-04 13:29:30 +0100

    Day 1 part 2

--- Days/Day01/Day1Second.php
+++ Days/Day01/Day1Second.php
@@ -6,6 +6,20 @@ class Day1Second extends Day1First implements \Days\Day
 {
     public function run(array $input): int|string
     {
-        return 'Not Implemented';
+        $max = [0, 0, 0];
+        $elf = 0;
+        foreach ($input as $item) {
+            if (empty($item)) {
+                $min = array_search(min($max), $max);
+                $max[$min] = max($max[$min], $elf);
+
+                $elf = 0;
+                continue;
+            }
+
+            $elf += $item;
+        }
+
+        return array_sum($max);
     }
 }
